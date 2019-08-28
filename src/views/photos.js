50cc3febd0b54b0da5402bb21e626ebd794400db
zhangshuang-h <781792980@qq.com>
2019-08-28 15:15:04 +0800

    react重写
    Signed-off-by: zhangshuang-h <781792980@qq.com>

--- src/views/photos.js
+++ src/views/photos.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react'
+import { Carousel } from 'antd'
 import '../css/photos.css'
+import 'antd/lib/carousel/style/css'
 export default class Photos extends Component {
   render() {
+    function onChange(a){
+      console.log(a)
+    }
     return (
       <div className='photos'>
-        photos
+         <Carousel afterChange={onChange} autoplay>
+          <div>
+              <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+         </Carousel>
       </div>
     )
   }
